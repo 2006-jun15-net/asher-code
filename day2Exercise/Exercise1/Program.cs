0aed476d85a8b70eaa918936edcec81832ac1ef3
Asher Williams <asher.williams1998@gmail.com>
2020-06-16 17:00:28 -0500

    Completed Exercise 1

---
+++ day2Exercise/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] step = new char[8] {' ',' ',' ',' ',' ',' ',' ','#'};
+
+            int stepCount = 6;
+            while(stepCount > -1)
+            {
+                string currentStep = "";
+                for(int i = 0; i < step.Length; i++)
+                {
+                    currentStep += step[i];
+                }
+                Console.WriteLine(currentStep);
+                step[stepCount] = '#';
+                stepCount--;
+            }
+
+            string lastStep = "";
+            for(int i = 0; i < step.Length; i++)
+            {
+                lastStep += step[i];
+            }
+            Console.WriteLine(lastStep);
+        }
+    }
+}

c23b339e2e130da4698935ffe8331e217404ab76
Asher Williams <asher.williams1998@gmail.com>
2020-06-16 16:59:29 -0500

    Completed Exercise 3

---
+++ day2Exercise/Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string response = Console.ReadLine();
+            int N = Int32.Parse(response);
+            
+            Console.WriteLine(N);
+            while(true)
+            {
+                if(N == 1)
+                {
+                    break;
+                }
+                else if(N % 2 == 0)
+                {
+                    N = N / 2;
+                }
+                else
+                {
+                    N = N * 3 + 1;
+                }
+
+                Console.WriteLine(N);
+            }
+        }
+    }
+}

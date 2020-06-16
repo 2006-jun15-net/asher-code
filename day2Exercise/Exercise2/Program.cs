a104e2ae0b7b77fdf54e5bc13172f82975f0e629
Asher Williams <asher.williams1998@gmail.com>
2020-06-16 17:00:00 -0500

    Completed Exercise 2

---
+++ day2Exercise/Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many steps? ");
+            string numberOfSteps = Console.ReadLine();
+            int steps = Int32.Parse(numberOfSteps);
+            int currentStep = 1;
+            
+            while(currentStep < steps + 1)
+            {
+                string step = "";
+                for(int i = 0; i < (steps - currentStep); i++)
+                {
+                    step = step + " ";
+                }
+                for(int i = 0; i < currentStep; i++)
+                {
+                    step = step + "#";
+                }
+                Console.WriteLine(step);
+                currentStep++;
+            }
+        }
+    }
+}
